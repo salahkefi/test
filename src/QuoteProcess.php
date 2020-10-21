0faaf1aa2dc4f37e501f231bfef5c19beaf67b3a
Kefi Salah <mohamedsalah.kefi@sofrecom.com>
2020-10-21 23:28:45 +0100

    [main] - Refactor QuoteProcess class

--- src/QuoteProcess.php
+++ src/QuoteProcess.php
@@ -18,39 +18,84 @@ class QuoteProcess
             $usefulObject = SiteRepository::getInstance()->getById($quote->siteId);
             $destinationOfQuote = DestinationRepository::getInstance()->getById($quote->destinationId);
 
-            if(strpos($text, '[quote:destination_link]') !== false){
-                $destination = DestinationRepository::getInstance()->getById($quote->destinationId);
-            }
+            $text = $this->patternsSummaryHtml($text,'[quote:summary_html]', $_quoteFromRepository);
+            $text = $this->patternsSummary($text,'[quote:summary]', $_quoteFromRepository);
+            $text = $this->patternsDestinationName($text,'[quote:destination_name]', $destinationOfQuote->countryName);
+            $text = $this->patternsDestinationLink($text,'[quote:destination_link]', $destinationOfQuote->countryName,$usefulObject->url,$_quoteFromRepository->id);
 
-            $containsSummaryHtml = strpos($text, '[quote:summary_html]');
-            $containsSummary     = strpos($text, '[quote:summary]');
-
-            if ($containsSummaryHtml !== false || $containsSummary !== false) {
-                if ($containsSummaryHtml !== false) {
-                    $text = str_replace(
-                        '[quote:summary_html]',
-                        Quote::renderHtml($_quoteFromRepository),
-                        $text
-                    );
-                }
-                if ($containsSummary !== false) {
-                    $text = str_replace(
-                        '[quote:summary]',
-                        Quote::renderText($_quoteFromRepository),
-                        $text
-                    );
-                }
-            }
-
-            (strpos($text, '[quote:destination_name]') !== false) and $text = str_replace('[quote:destination_name]',$destinationOfQuote->countryName,$text);
         }
-
-        if (isset($destination))
-            $text = str_replace('[quote:destination_link]', $usefulObject->url . '/' . $destination->countryName . '/quote/' . $_quoteFromRepository->id, $text);
-        else
-            $text = str_replace('[quote:destination_link]', '', $text);
 
         return $text;
 
+    }
+
+    /**
+     * Méthode pour gérer les placeholders de type [quote:summary_html]
+     * @param string $text
+     * @param string $pattern
+     * @param Quote $quote
+     * @return string $text
+     */
+    private function patternsSummaryHtml($text,$pattern,$quote)
+    {
+        $containsSummary = strpos($text, $pattern);
+        if ($containsSummary !== false) {
+            $text = str_replace(
+                '[quote:summary_html]',
+                Quote::renderText($quote),
+                $text
+            );
+        }
+        return $text;
+    }
+
+    /**
+     * Méthode pour gérer les placeholders de type [quote:summary]
+     * @param string $text
+     * @param string $pattern
+     * @param Quote $quote
+     * @return string $text
+     */
+    private function patternsSummary($text,$pattern,$quote)
+    {
+        $containsSummary = strpos($text, $pattern);
+        if ($containsSummary !== false) {
+            $text = str_replace(
+                '[quote:summary]',
+                Quote::renderHtml($quote),
+                $text
+            );
+        }
+        return $text;
+    }
+
+    /**
+     * @param string $text
+     * @param string $pattern
+     * @param string $countryName
+     * @return string $text
+     */
+    private function patternsDestinationName($text,$pattern,$countryName)
+    {
+        (strpos($text, $pattern) !== false) and $text = str_replace($pattern,$countryName,$text);
+        return $text;
+    }
+
+
+    /**
+     * @param string $text
+     * @param string $pattern
+     * @param string $countryName
+     * @param string $url
+     * @param integer $id
+     * @return string|string[] $text;
+     */
+    private function patternsDestinationLink($text,$pattern,$countryName,$url,$id)
+    {
+        if (strpos($text, $pattern) !== false)
+            $text = str_replace($pattern, $url . '/' . $countryName . '/quote/' . $id, $text);
+        else
+            $text = str_replace($pattern, '', $text);
+        return $text;
     }
 }
